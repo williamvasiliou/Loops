b3ab275b25b5d3872a1e84492b9e3ee68764f290
williamvasiliou <195391054+williamvasiliou@users.noreply.github.com>
2025-07-17 15:55:27 +0000

    Add files via upload

--- sum.h
+++ sum.h
@@ -1,4 +1,23 @@
 #include <stdint.h>
+#include <sys/random.h>
+
+uint8_t byte() {
+	static uint8_t i[1];
+	ssize_t nread = getrandom(i, 1, 0);
+	while (nread != 1) {
+		nread = getrandom(i, 1, 0);
+	}
+	return i[0];
+}
+
+uintmax_t integer() {
+	uintmax_t i = byte();
+	for (size_t j = 1; j < sizeof(uintmax_t); ++j) {
+		i <<= 8;
+		i |= byte();
+	}
+	return i;
+}
 
 void splice(uint8_t *out, const uint8_t *in, size_t index, size_t size) {
 	if (size > 0) {
